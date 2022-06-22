0bec008f231019208d5c3f4e323fe1eff643eb55
Sk0rpi0o <brown.stefan@gmail.com>
2022-06-22 12:29:55 -0400

    Version 3.0 - countdown down w/out alignments

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 // ending date minus current date for countdown
 
-const newWebsite = '31 Oct 2022'
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+ 
+const   newWebsite = "31 Oct 2022";
 
 function countdown() {
   const newWebsiteDate = new Date(newWebsite);
   const currentDate = new Date();
 
-  const seconds = (newWebsiteDate - currentDate) / 1000;
+  const totalSeconds = (newWebsiteDate - currentDate) / 1000;
 
-  const days = Math.floor(seconds / 3600 / 24);
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 24 % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = seconds;
+
+  console.log(days, hours, minutes, seconds);
 }
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
 // inital call
 countdown();
 
